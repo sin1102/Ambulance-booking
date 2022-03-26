754a1bc2e7e82cd63e38e442bf34bde2990398e2
TienMinh <minhphm23@gmail.com>
2022-03-26 20:36:49 +0700

    xém add map

--- app/src/main/java/com/example/ambulancebooking/MainActivity.kt
+++ app/src/main/java/com/example/ambulancebooking/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.ambulancebooking
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.libraries.places.api.Places
 
 class MainActivity : AppCompatActivity() {
+
+    var isChecked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var checkAnim = findViewById<LottieAnimationView>(R.id.lottieAmbulance)
     }
+
 }
