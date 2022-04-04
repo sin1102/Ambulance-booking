066dc8fa0c82d241ac2b28b9401ff4e8a43d0ced
anhtr <anhtrungdinh210@gmail.com>
2022-04-04 20:27:19 +0700

    Profile, Sign Up

--- app/src/main/java/com/example/ambulancebooking/ProfileActivity.kt
+++ app/src/main/java/com/example/ambulancebooking/ProfileActivity.kt
@@ -2,11 +2,10 @@ package com.example.ambulancebooking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class SignUpActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_profile)
     }
 }
